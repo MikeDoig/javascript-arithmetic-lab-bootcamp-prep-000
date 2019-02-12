df3614dab8d8ba601c006925bad31733772bc178
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 18:35:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+function subtract(a,b) {
+  return parseInt(a) - parseInt(b);
+}
+function multiply(a,b) {
+  return parseInt(a) * parseInt(b);
+}
+function divide(a,b) {
+  return parseInt(a) / parseInt(b);
+}
+function inc(a) {
+  return parseInt(a + 1);
+}
+function dec(a) {
+  return parseInt(a - 1);
+}
+function makeInt(a) {
+  return parseInt(a);
+}
